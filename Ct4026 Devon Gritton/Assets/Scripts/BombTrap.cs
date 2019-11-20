4537da28824878987e4986b394ef3e126ba7996a
DevonGritton <55791362+DevonGritton@users.noreply.github.com>
2019-11-20 13:14:44 +0000

    Implemented Bomb
    Added a stationary bomb trap that will detonate when users come close to it. I aim to make the bomb fall from pipes that can damage the player with explosions

---
+++ Ct4026 Devon Gritton/Assets/Scripts/BombTrap.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombTrap : MonoBehaviour
+{
+    public float ExplosionStrength = 30;
+    public float ExplosionRadius = 5;
+    public float BombTimer = 3;
+
+    private bool bombon = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bombon)
+        {
+            BombTimer -= Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        bombon = true;
+        
+
+        if (BombTimer <= 0)
+        {
+
+
+            Collider[] colliders = Physics.OverlapSphere(transform.position, ExplosionRadius);
+            foreach (Collider collider in colliders)
+            {
+                Rigidbody rigid = collider.GetComponent<Rigidbody>();
+                if (rigid != null)
+                {
+                    rigid.AddExplosionForce(ExplosionStrength, transform.position, ExplosionRadius, 0, ForceMode.Impulse);
+                }
+            }
+            Destroy(gameObject);
+        }
+    }
+}
