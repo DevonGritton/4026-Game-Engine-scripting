969e75ebf3c63d93f21c7e834ab9a5f266b62d62
DevonGritton <55791362+DevonGritton@users.noreply.github.com>
2019-11-24 16:57:31 +0000

    Created a wall prefab
    Ammended bomb script to count down when only the player enters the collider as well as begin to create prefabs for the design of my level

--- Ct4026 Devon Gritton/Assets/Scripts/BombTrap.cs
+++ Ct4026 Devon Gritton/Assets/Scripts/BombTrap.cs
@@ -4,26 +4,25 @@ using UnityEngine;
 
 public class BombTrap : MonoBehaviour
 {
-    public float ExplosionStrength = 30;
-    public float ExplosionRadius = 5;
-    public float BombTimer = 3;
-
-    private bool bombon = false;
+    public float ExplosionStrength = 30.0f;
+    public float ExplosionRadius = 5.0f;
+    public float BombTimer = 3.0f;
+    float countdown;
 
     // Update is called once per frame
     void Update()
     {
-        if (bombon)
-        {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+            if (collision.gameObject.tag == "Player")
+            {
             BombTimer -= Time.deltaTime;
         }
     }
-
     private void OnTriggerEnter(Collider other)
     {
-        bombon = true;
-        
-
         if (BombTimer <= 0)
         {
 
