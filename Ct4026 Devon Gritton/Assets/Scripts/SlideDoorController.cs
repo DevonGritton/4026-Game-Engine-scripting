25ae1c8777d3d18a353fdcf2b40ece5e1d383758
DevonGritton <55791362+DevonGritton@users.noreply.github.com>
2019-11-27 13:15:43 +0000

    Fixed animations
    Added animations for new doors and completed the first section of the level as well as fix the brightness slider for the menu

---
+++ Ct4026 Devon Gritton/Assets/Scripts/SlideDoorController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideDoorController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    public GameObject DoorObject;
+
+    private bool DoorIsUp;
+    private bool buttonPressed;
+
+    void OnMouseOver()
+    {
+        buttonPressed = true;
+    }
+
+    void OnMouseDown()
+    {
+        DoorIsUp = !DoorIsUp;
+        DoorObject.GetComponent<Animator>().SetBool("DoorIsUp", DoorIsUp);
+    }
+
+    void OnMouseExit()
+    {
+        buttonPressed = false;
+    }
+    void OnGUI()
+    {
+        if (buttonPressed == true)
+        {
+            GUI.Box(
+                new Rect(
+                    Screen.width / 2 - 100,
+                    Screen.height / 2 - 25,
+                    200,
+                    50),
+                    "Press Button"
+                    );
+        }
+    }
+}
+
