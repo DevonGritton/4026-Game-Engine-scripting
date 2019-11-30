7a37413964c02cae5c653f47e62dc482982ced82
DevonGritton <55791362+DevonGritton@users.noreply.github.com>
2019-11-30 14:51:12 +0000

    Turret
    Began working ona turret section that will be the end part for the game level

---
+++ Ct4026 Devon Gritton/Assets/Scripts/DartProjectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DartProjectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.collider.CompareTag("Enviroment"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Ct4026 Devon Gritton/Assets/Scripts/DartTurret.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DartTurret : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject m_goDartPrefab;
+
+    [SerializeField]
+    private float m_fDartSpeed = 100.0f;
+
+    public Transform firepoint;
+    public float fireRate = 0.5f;
+    public float firecountdown = 1f;
+    private Transform target;
+    public float range = 80f;
+    public string targetTag = "Target";
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        firecountdown -= Time.deltaTime;
+        if (firecountdown <= 0)
+        {
+            ShootBullet();
+            firecountdown = 1f;
+        }
+        if (target == null)
+            return;
+    }
+    void UpdateTarget ()
+    {
+        GameObject[] Targets = GameObject.FindGameObjectsWithTag(targetTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestTarget = null;
+        foreach (GameObject target in Targets)
+        {
+            float distancetoTarget = Vector3.Distance(transform.position, target.transform.position);
+            if (distancetoTarget < shortestDistance)
+            {
+                shortestDistance = distancetoTarget;
+                nearestTarget = target;
+            }
+        }
+
+        if (nearestTarget != null && shortestDistance <= range)
+        {
+            target = nearestTarget.transform;
+        }
+
+
+    }
+    private void ShootBullet()
+    {
+        GameObject goSpawnedDart = Instantiate(m_goDartPrefab, firepoint.position, firepoint.rotation);
+        goSpawnedDart.transform.position = transform.position;
+        goSpawnedDart.transform.Translate(transform.forward);
+
+        Rigidbody rbDartbody = goSpawnedDart.GetComponent<Rigidbody>();
+        if (rbDartbody != null)
+        {
+            rbDartbody.velocity = transform.forward * m_fDartSpeed;
+        }
+        Destroy(goSpawnedDart, 1.0f);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, range);
+        Gizmos.color = Color.blue;
+    }
+}
